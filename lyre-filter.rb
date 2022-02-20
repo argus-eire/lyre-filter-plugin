9f123a3262479b799d1bcb15b417655af32c4972
Argus McIntire <100048156+argus-eire@users.noreply.github.com>
2022-02-20 00:06:07 -0500

    Create lyre-filter.rb

---
+++ lyre-filter.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+Gem::Specification.new do |spec|
+  spec.name        = "lyre-filter"
+  spec.version     = "0.1.0"
+  spec.authors     = ["Argus McIntire"]
+  spec.email       = ["argus.mcintire@protonmail.com"]
+  
+  spec.summary     = "get this bleeding generator working"
+  
+  spec.files       = `git ls-files -z`.split("\x0").select { |f| f.match(%r!^(lib)!i) }
+
+  spec.add_runtime_dependency "jekyll", "~> 4.1"
+  spec.add_runtime_dependency "word_wrap", "~>1.0"
+end
